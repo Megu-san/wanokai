beb4df85a83649fa725e0e6c5f5524caf81a4967
Megu-san <megi.zaki@gmail.com>
2022-10-07 23:33:07 +0900

    usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many attendances
+  has_many reports
+  
+  validates :name, presence: true
+  validates :birth_date, presence: true
+end
