d0894f1b859f48dc7786425c79620648b866f825
Megu-san <megi.zaki@gmail.com>
2022-10-08 22:46:32 +0900

    usersテーブルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many attendances
-  has_many reports
+  # has_many activities
+  # has_many reports
   
   validates :name, presence: true
   validates :birth_date, presence: true
