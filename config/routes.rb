cd51536d31a63b2d20167492211cf3697ff15984
Megu-san <megi.zaki@gmail.com>
2022-10-09 20:55:42 +0900

    トップ・新規登録・ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get 'activities/index'
   devise_for :users
   root to: "activities#index"
+  resources :activities, only: [:new, :create, :show, :edit, :update]
+  resources :reports, only: [:new, :create, :show, :edit, :update]
 end
